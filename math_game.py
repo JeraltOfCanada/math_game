b7c936144aacbe701ed3711267dd91c7ae6eef5b
Jordan M <jordan.moltzan@pm.me>
2026-04-21 13:01:55 -0600

    Added name=main and commented on loop

--- math_game.py
+++ math_game.py
@@ -1,19 +1,22 @@
 import utils.unix_time_randomizer as roll
 
-num1, num2 = roll.get_numbers()
+if __name__ == "__main__":
 
-correct_answer = (num1 + num2)
-counter = 0
+    num1, num2 = roll.get_numbers()
 
-while True:
-    print(f"{num1} + {num2} = ")
-    answer = int(input("Sum: "))
-    counter = counter + 1
-    if answer == correct_answer:
-        print("CORRECT!")
-        break
-    if answer != correct_answer and counter <= 5:
-        print("Try again!")
-    else:
-        print("You lost!")
-        break
+    correct_answer = (num1 + num2)
+    counter = 0
+
+# Loop requests input until user gets it right, or exceeds 5 tries
+    while True:
+        print(f"{num1} + {num2} = ")
+        answer = int(input("Sum: "))
+        counter = counter + 1
+        if answer == correct_answer:
+            print("CORRECT!")
+            break
+        if answer != correct_answer and counter <= 5:
+            print("Try again!")
+        else:
+            print("You lost!")
+            break
